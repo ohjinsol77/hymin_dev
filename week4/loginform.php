b8ea2fe51c5a42bfc886c61b4abd5ff21c086b69
hyeonmin29 <hymin8804@gmail.com>
2022-10-19 20:50:12 +0900

    20221019

---
+++ week4/loginform.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title> 로그인 테스트 </title>
+<meta charset = "utf-8">
+</head>
+
+<body>
+<form action = "login.php" method="post">
+ID : <input type = "text" name = "id"/></br>
+PW : <input type = "password" name = "pw"/></br></br>
+<input type = "submit" name = "login" value = "로그인">
+
+</form>
+</body>
+</html>
---
+++ week4/logout.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+session_destroy();
+?>
+<script>
+	//알림창에서 로그아웃 띄우기
+    alert("로그아웃 되셨습니다.");
+	//로그인폼으로 돌아가기
+    location.replace('loginform.php');
+</script>
