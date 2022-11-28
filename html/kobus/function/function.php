40187943869e4557c07a39762bb2ce80abb3bc25
hyeonmin29 <hymin8804@gmail.com>
2022-11-28 18:37:47 +0900

    GitErrorTest

--- html/kobus/function/function.php
+++ html/kobus/function/function.php
@@ -1,9 +1,6 @@
 <?php
 error_reporting( E_ALL );
 ini_set( "display_errors", 1 );
-
-
-
 /* mysqli 쿼리로 바로 실행시키는건 부적합 -> 교체해야함 */
 class database{
 	private $strHost = 'localhost';
@@ -16,32 +13,6 @@ class database{
 	public function __construct(){
 		$this->db = mysqli_connect($this->strHost, $this->strId, $this->strPw, $this->strTable, $this->strPort, $this->strSock);
 	}
-
-	public function fnCommit(){
-		if(mysqli_query($this->db,'commit')){
-			return true;
-		}else{
-			return false;
-		}
-
-	}
-
-	public function fnRollback(){
-		if(mysqli_query($this->db, 'rollback')){
-			return true;
-		}else{
-			return false;
-		}
-	}
-	
-	public function fnStart_trans(){
-		if(!mysqli_query($this -> db, 'set autocommit = 0') || !mysqli_query($this -> db, 'start transaction')) {
-			return false;
-		}else{
-			return true;
-		}
-	}
-
 }
 
 function fnAlert($strAlert , $strLocation) {
@@ -50,4 +21,29 @@ function fnAlert($strAlert , $strLocation) {
 
 	echo $strAlrt;
 }
+
+function gap_time($dtTime) {
+	
+	$dtTime = strtotime ($dtTime);
+	$end_time = strtotime (date("H:i:s"));
+	
+	if ($dtTime < $end_time) {
+		$diff = $end_time -$dtTime;
+	} else {
+		$diff = "-".$dtTime - $end_time;
+	}
+
+	$hours = floor ($diff/3600);
+	$diff = $diff - ($hours*3600);
+	$min = floor ($diff/60);
+	$sec = $diff - ($min*60);
+	return sprintf ("%02d:%02d:%02d", $hours, $min, $sec); 
+}
+
+class DBexception extends exception {
+}
+
+
+
+
 ?>
